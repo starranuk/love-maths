27a70922fa9fa725a75366a499335e1ef4dc3198
starranuk <starran@live.co.uk>
2021-11-09 12:53:07 +0000

    display addition question - works

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,12 +10,13 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert ("You have pressed submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert (`You have pressed ${gameType}`);
+                runGame(gameType);
 
             }
             
         });
     }
+    runGame("addition");
 });
 
 /**
@@ -23,10 +24,17 @@ document.addEventListener("DOMContentLoaded", function() {
  * and after the user's answer has been processed
  */
 
-function runGame() {
+function runGame(gameType) {
     // Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) +1;
     let num2 = Math.floor(Math.random() * 25) +1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 
 }
 
@@ -46,7 +54,11 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 
 }
 
